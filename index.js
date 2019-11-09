7ffd8ee1383d742c2b97884befd4892a7c57a8a4
Paul <nissenbrau@gmail.com>
2019-11-09 14:38:12 -0700

    fixed toggle switch

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function handleTheme() {
   $('#toggleTheme').click(function(){
     $('body').toggleClass('js-lightmodeBackground');
     $('section, nav, footer').toggleClass('js-lightmodeSection');
-    $('h1, a, p').toggleClass('js-lightmodeText');
+    $('h1, a, p, i').toggleClass('js-lightmodeText');
+    $('.fa-toggle-on').toggleClass('fa-flip-horizontal')
   });
 }
 
