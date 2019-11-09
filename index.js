e8999b4dfa4bc331fbcda1aeadf47904ca25c4b8
Paul <nissenbrau@gmail.com>
2019-11-09 11:35:08 -0700

    updated content, added responsive styling, added background pics. Need to add links, fix nav at mobile sizing, improve theme button, and add projects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function handleTheme() {
   $('#toggleTheme').click(function(){
     $('body').toggleClass('js-lightmodeBackground');
     $('section, nav, footer').toggleClass('js-lightmodeSection');
-    $('h1, a').toggleClass('js-lightmodeText');
+    $('h1, a, p').toggleClass('js-lightmodeText');
   });
 }
 
