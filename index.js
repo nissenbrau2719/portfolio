e1941326d6f09a25b24e0a48724e295b42f4fdb3
Paul <nissenbrau@gmail.com>
2019-11-17 17:09:21 -0700

    final tweaks on font sizes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function handleTheme() {
   $('#toggleTheme').click(function(){
     $('body').toggleClass('js-lightmodeBackground');
     $('section, nav, footer').toggleClass('js-lightmodeSection');
-    $('h1, a, p, i').toggleClass('js-lightmodeText');
+    $('h1, h2, a, p, i').toggleClass('js-lightmodeText');
     $('.fa-toggle-on').toggleClass('fa-flip-horizontal')
   });
 }
